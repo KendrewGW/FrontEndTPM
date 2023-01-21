57e384b0a258272a90571dad5381a00c38efba91
u-rated <ihsaan.hardyanto@gmail.com>
2023-01-21 21:52:55 +0700

    done faq

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+// function openForm() {
+//   document.getElementById("myForm").style.display = "block";
+// }
+
+// function closeForm() {
+//   document.getElementById("myForm").style.display = "none";
+// }
+
+/*==================== Skills ====================*/
+const skillsContent = document.getElementsByClassName('skills__content'),
+    skillsHeader = document.querySelectorAll('.skills__header')
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if(itemClass === 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((el)=>{
+    el.addEventListener('click', toggleSkills)
+})
